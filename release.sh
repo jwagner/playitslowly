a5a1fddb1a971862826252a15d428f9b454077b9
Jonas Wagner <jonas@29a.ch>
2011-07-03 15:47:48 +0200

    Updated release script to build deb

--- release.sh
+++ release.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+rm dist/*
 python setup.py sdist
+sudo make builddeb
+cp ../playitslowly_*.deb dist
+#sudo rm ../playitslowly_*
 rsync -vrt --copy-links dist/* 29a.ch:/var/www/29a.ch/playitslowly/
 
