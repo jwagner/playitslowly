85f3cfc4c1c9e2474bfcebdcf86e05d183868cb8
Jonas Wagner <jonas@29a.ch>
2012-03-13 20:18:04 +0100

    Removed deb dir, fixes #4

--- release.sh
+++ release.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 rm dist/*
-sudo make builddeb
-cp ../playitslowly_*.deb dist
-sudo rm ../playitslowly_*
+#http://lpenz.github.com/articles/debgit/index.html
+echo "implement DEB packaging using git-buildpackage"
+#sudo make builddeb
+#cp ../playitslowly_*.deb dist
+#sudo rm ../playitslowly_*
 python setup.py sdist
 rsync -vrt --copy-links dist/* 29a.ch:/var/www/29a.ch/playitslowly/
 
